f744a03c9910c21436e5a1c94c208af02d8ebfa6
Daniel Park <daniel@danubers.com>
2017-02-15 18:20:15 +1000

    Basic project

---
+++ com.danubers.qld-electricity/Startup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace com.danubers.qld_electricity
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //Default Files MUST be called before static files https://docs.microsoft.com/en-us/aspnet/core/fundamentals/static-files
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+        }
+    }
+}
---
+++ com.danubers.qld-electricity/wwwroot/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by daniel on 13/2/17.
+ */
+console.log("hello!");
